418703e784418e3f9ea22faf57a8ee61598e796b
Charlie Peters <chpeters13@gmail.com>
2017-11-26 18:10:32 -0500

    added app homepage

--- assets/src/components/App/App.js
+++ assets/src/components/App/App.js
@@ -1,34 +1,46 @@
 import React, { Component } from 'react'
 import './App.css'
-import Grid from '../Grid'
-import MessageSection from '../MessageSection'
+// import Grid from '../Grid'
+// import MessageSection from '../MessageSection'
 
-const arr = [
-  'empty','empty','empty','empty','empty','empty','empty','empty','empty','empty',
-  'empty','empty','empty','empty','empty','empty','empty','empty','empty','empty',
-  'empty','empty','empty','empty','empty','empty','empty','empty','empty','empty',
-  'empty','empty','empty','empty','empty','empty','empty','empty','empty','empty',
-  'empty','empty','empty','empty','empty','empty','empty','empty','empty','empty',
-  'empty','empty','empty','empty','empty','empty','empty','empty','empty','empty',
-  'empty','empty','empty','empty','empty','empty','empty','empty','empty','empty',
-  'empty','empty','empty','empty','empty','empty','empty','empty','empty','empty',
-  'empty','empty','empty','empty','empty','empty','empty','empty','empty','empty',
-  'empty','empty','empty','empty','empty','empty','empty','empty','empty','empty'
-]
-const msgs = [
-  {text: 'Hello there!', type: 'self'},
-  {text: 'Yooo!', type: 'opponent'},
-  {text: 'Do you want to play battleship! This is a really long message that should line wrap a few times', type: 'self'}
-]
+// const arr = [
+//   'empty','empty','empty','empty','empty','empty','empty','empty','empty','empty',
+//   'empty','empty','empty','empty','empty','empty','empty','empty','empty','empty',
+//   'empty','empty','empty','empty','empty','empty','empty','empty','empty','empty',
+//   'empty','empty','empty','empty','empty','empty','empty','empty','empty','empty',
+//   'empty','empty','empty','empty','empty','empty','empty','empty','empty','empty',
+//   'empty','empty','empty','empty','empty','empty','empty','empty','empty','empty',
+//   'empty','empty','empty','empty','empty','empty','empty','empty','empty','empty',
+//   'empty','empty','empty','empty','empty','empty','empty','empty','empty','empty',
+//   'empty','empty','empty','empty','empty','empty','empty','empty','empty','empty',
+//   'empty','empty','empty','empty','empty','empty','empty','empty','empty','empty'
+// ]
+// const msgs = [
+//   {text: 'Hello there!', type: 'self'},
+//   {text: 'Yooo! Wuss up?', type: 'opponent'},
+//   {text: 'Do you want to play battleship! This is a really long message that should line wrap a few times', type: 'self'}
+// ]
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className='container'>
+//         <div className='table'>
+//           <Grid size={300} elements={arr} />
+//         </div>
+//         <MessageSection messages={msgs}/>
+//       </div>
+//     );
+//   }
+// }
 
 class App extends Component {
   render() {
     return (
       <div className='container'>
-        <div className='table'>
-          <Grid size={300} elements={arr} />
-        </div>
-        <MessageSection messages={msgs}/>
+        <h1 style={{textAlign: 'center'}}>Battleship</h1>
+        <div style={{maxWidth: '300px', margin: '0 auto'}}><label for='code'>Input Table Code: </label><input type='text'/></div>
+        <button style={{margin: '15px auto', display: 'block'}} type="button">New Table</button>
       </div>
     );
   }
